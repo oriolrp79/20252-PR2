fb135e4f2c92ab0cd1b496a9f88b5c8327165587
Oriol Roqué Prat <oriolrp79@gmail.com>
2026-05-18 00:37:08 +0200

    tot js i ara css

--- js/index.js
+++ js/index.js
@@ -1,18 +1,51 @@
 /**
  * index.js — Lógica de la página de login
- */
+
+Utilitzem Live Server per executar el projecte en el navegador, así el localStorage funciona bé entre pàgines
+*/
+
 
 /*Este script debe de gestionar el login de los usuarios.*/
 
 /* Añadir las funciones que consideréis necesarias*/
 
+//declarem les variables, recuperant la info de localStorage i del DOM
+const storedUsername = localStorage.getItem('username');
+const storedPassword = localStorage.getItem('password');
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const errorMsg = document.getElementById('errorMsg');
+//recuperem la info de si està loguejat l'usuari
+const isLogged = sessionStorage.getItem('isLogged');
+
+    //COMPROvació de que la info ha quedat guardada en el localStorage
+    //console.log(storedUsername);
+    //console.log(storedPassword);
+
     // Si hay un usuario logeado, redirigimos a la página indice de Pokemons
     //...
-
+if (isLogged) {
+    window.location.href = 'anime.html';
+} else {
     // Si no hay un usuario logeado, comprobamos datos de login
     //...
+    document.getElementById('loginButton').addEventListener('click', () => {
+        if ((storedUsername===usernameInput.value) && (storedPassword===passwordInput.value)) {
+            // si usuari i contrsenya son correctes guardem que l'usuari està loguejat i redirigim a anime.html
+            isLogged=true;
+            sessionStorage.setItem('isLogged', isLogged); //utilitzem sessionStorage perquè si el navegador es tanca el login s'ha de tornar a fer
+            window.location.href = 'anime.html';
+        } else { 
+            //en cas contrari missatge d'error
+            errorMsg.textContent = 'Usuari o contrasenya incorrectes';
+            errorMsg.style.display = 'block';
+        }
+        
+    });
 
     // Al pulsar el boton redirigimos a la página de registro
-    //...
-
+    document.getElementById('newUserButton').addEventListener('click', () => {
+        window.location.href = 'registro.html';
+    });
+}
     /* Añadir las funciones que consideréis necesarias*/
--- js/registro.js
+++ js/registro.js
@@ -4,3 +4,159 @@
  */
 
 /* Añadir las funciones que consideréis necesarias*/
+
+//declaro les variables que necessito, agafant elements del DOM
+const nameUser = document.getElementById('name');
+const surnameUser = document.getElementById('surname');
+const addressUser = document.getElementById('address');
+const cityUser = document.getElementById('city');
+const postalCodeUser = document.getElementById('postalCode');
+const emailUser = document.getElementById('email');
+const usernameUser = document.getElementById('username');
+const passwordUser = document.getElementById('password');
+const password2User = document.getElementById('password2');
+
+//agafo també els elements per missatges d'error
+const nameError = document.getElementById('nameError');
+const surnameError = document.getElementById('surnameError');
+const addressError = document.getElementById('addressError');
+const cityError = document.getElementById('cityError');
+const postalCodeError = document.getElementById('postalCodeError');
+const emailError = document.getElementById('emailError');
+const usernameError = document.getElementById('usernameError');
+const passwordError = document.getElementById('passwordError');
+const password2Error = document.getElementById('password2Error');
+
+//faig una funció per mostrar les ciutats i poder-les seleccionar com a desplegable
+function selectCity(){
+    cities.forEach(city => {
+        const option = document.createElement('option');
+        option.value = city.name;
+        option.textContent = city.name;
+        cityUser.appendChild(option);   
+    });
+}
+selectCity(); //crido la funció
+
+//escoltem l'event de quan se selecciona una ciutat i busquem el codi postal associat per escriure'l en l'input
+cityUser.addEventListener('change', () => {
+    const selectedCity = cities.find(city => city.name === cityUser.value);
+
+    if (selectedCity) {
+        postalCodeUser.value = selectedCity.postalCode;
+        postalCodeUser.setAttribute('value', selectedCity.postalCode);
+    } else {
+        postalCodeUser.value = "";
+    }
+});
+
+// fem el pas contrari, quan s'escriu un codi postal comprobem que existeixi a la taula cities i posem la ciutat que li toca
+postalCodeUser.addEventListener('input', () => {
+    const writtenPostalCode = postalCodeUser.value;
+    if (writtenPostalCode.length === 5) {
+        const linkedCity = cities.find(city => city.postalCode === writtenPostalCode);
+        if (linkedCity) {
+            cityUser.value = linkedCity.name;
+        } 
+    }
+
+});
+
+//escoltem l'event de quan escribim el correu, i a la que es detecta el símbol @ se li afegeix el domini de la uoc
+emailUser.addEventListener('input', () => {
+    const writtenEmail = emailUser.value;
+    if (writtenEmail.includes('@')) {
+        emailUser.value= writtenEmail+'uoc.edu';
+    }
+
+});
+
+//provem el localStorage per veure si guarda i comproba bé la informació del username
+//localStorage.setItem('username', "prova");
+
+// escoltem l'event d'introducció de Username i declarem una variable per saber si el que s'ha introduït és vàlid o noo
+let validUsername= true;
+usernameUser.addEventListener('input', () => {
+    const writtenUsername = usernameUser.value;
+    if (writtenUsername===localStorage.getItem('username')){
+        usernameError.textContent = 'El nom d\'usuari ja existeix';
+        validUsername=false;
+    } else { 
+        usernameError.textContent = '';
+        validUsername=true;
+    }
+});
+
+//escoltem l'event d'introducció del password i declarem variable per saber si el password és vàlid segons les condicions que hem posat
+let validPassword=false;
+passwordUser.addEventListener('input', () => {
+    const writtenPassword = passwordUser.value;
+    // logica amb .test() consultada a https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test
+    // per comprovar si el password conté els caràcters que volem
+    const numbers= /\d/.test(writtenPassword);
+    const letters= /[a-zA-Z]/.test(writtenPassword);
+    const specialChar= /[!@#$%^&*()_+{}|;:,.<>?]/.test(writtenPassword);
+  
+    if ((writtenPassword.length>=8)&&(numbers) && (letters) && (specialChar)) {
+        validPassword=true;
+    } else {
+        validPassword=false;
+    }
+    
+});
+
+
+//escoltem el click del botó de guardar les dades del formulari, i comprobem una a una que les dades siguin vàlides, i si no ho son mostrem missatge d'error
+document.getElementById('saveButton').addEventListener('click', () => {
+    if (nameUser.value===""){
+        nameError.textContent = 'El nom no pot estar buit';
+    } else { nameError.textContent = '';}
+    if (surnameUser.value===""){
+        surnameError.textContent = 'El cognom no pot estar buit';
+    } else { surnameError.textContent = '';}
+    if (addressUser.value===""){
+        addressError.textContent = 'L\'adreça no pot estar buida';
+    } else { addressError.textContent = '';}
+    if (cityUser.value===""){
+        cityError.textContent = 'La ciutat no pot estar buida';
+    } else { cityError.textContent = '';}
+    if (postalCodeUser.value===""){
+        postalCodeError.textContent = 'El codi postal no pot estar buit';
+    } else { postalCodeError.textContent = '';}
+    if (emailUser.value===""){
+        emailError.textContent = 'L\'email no pot estar buit';
+    } else { emailError.textContent = '';}
+    if (usernameUser.value===""){
+        usernameError.textContent = 'El nom d\'usuari no pot estar buit';
+    } else { usernameError.textContent = '';}
+    if (!validPassword){
+        passwordError.textContent = 'La contrasenya ha de tenir mínim 8 caracteres, incluir números, letras y caracteres especiales';
+    } else { passwordError.textContent = '';}
+    if (password2User.value===""){
+        password2Error.textContent = 'La confirmació de la contrasenya no pot estar buida';
+    } else { password2Error.textContent = '';}
+
+    // si totes les dades son vàlides guardarem en el localStorage tota la info i tornarem cap a la pàgina index.html
+    if ((nameUser.value.length>0)&&(surnameUser.value.length>0)&&(addressUser.value.length>0)&&(cityUser.value.length>0)&&(postalCodeUser.value.length>0)&&(emailUser.value.length>0)&&(usernameUser.value.length>0)&&(validUsername)&&(passwordUser.value===password2User.value)&&(validPassword)){
+        // localstorage
+        localStorage.setItem('name', nameUser.value);
+        localStorage.setItem('surname', surnameUser.value);
+        localStorage.setItem('address', addressUser.value);
+        localStorage.setItem('city', cityUser.value);
+        localStorage.setItem('postalCode', postalCodeUser.value);
+        localStorage.setItem('email', emailUser.value);
+        localStorage.setItem('username', usernameUser.value);
+        localStorage.setItem('password', passwordUser.value);
+        //console.log(localStorage.getItem('username'));
+        //console.log(localStorage.getItem('password'));
+        window.location.href = 'index.html';
+    }
+});
+
+//escoltem el click en el botó de tornar cap a la pagina index.html sense validar el formulari ni guardar res
+document.getElementById('backToLoginButton').addEventListener('click', () => {
+    window.location.href = 'index.html';
+});
+
+
+ 
